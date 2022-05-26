0256f0a643a645070529ef8e718935cb80fda25c
Ian Hinder <ian.hinder@manchester.ac.uk>
2022-05-26 23:35:02 +0100

    cs_user_coupling.c: Remove call to cs_luma_coupling_define

--- src/user/cs_user_coupling.c
+++ src/user/cs_user_coupling.c
@@ -126,12 +126,6 @@ cs_user_saturne_coupling(void)
 void
 cs_user_luma_coupling(void)
 {
-	// variables to couple
-	// v = velocity
-	// t = temperature
-
-	int verbosity = 1;
-	cs_luma_coupling_define("LEFT", "Left", "box[0.54, 0.01, 0, 0.55, 1.01, 0.05]", 'L', true, 1.0, verbosity, 0, "v", "v");//"Left", box[0.54, 0.01, 0.0, 0.55, 1.01, 0.05]//"B_mid"
 
 }
 
